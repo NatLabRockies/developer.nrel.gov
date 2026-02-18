8a962742797fa90735942998006b3c00d71270e0
Nick Muerdter <12112+GUI@users.noreply.github.com>
2026-02-18 16:33:49 -0700

    Fix broken swagger docs.
    The Swagger docs became broken due to the package updates in
    https://github.com/NatLabRockies/developer.nrel.gov/pull/445.
    
    It was broken with somewhat vague `x is not a function` errors from the
    buried swagger files. Based on a few GitHub issues, I suspect this was
    due to incompatibilities with other nested dependencies within Swagger
    where perhaps their version pinning isn't accounting for new behavior in
    new versions of the dependencies they are allowing.
    
    I was able to fix this by switching to the `swagger-ui-dist` version of
    the package, which includes all of the nested dependencies compiled
    against known working versions.
    
    While [Swagger
    documents](https://github.com/swagger-api/swagger-ui?tab=readme-ov-file#general):
    
    > We strongly suggest that you use `swagger-ui` instead of
    > `swagger-ui-dist` if you're building a single-page application, since
    > `swagger-ui-dist` is significantly larger.
    
    We're not really building a single page app here, and the size
    difference is likely not huge for us, since it's not like we're reusing
    React or other nested dependencies elsewhere on the page. Since we
    simply want a version of Swagger UI that works and we don't care about
    tree-shaking or the nested dependencies, using this precompiled version
    seems simpler for our static website use-case.

--- frontend/entrypoints/swagger.js
+++ frontend/entrypoints/swagger.js
@@ -1,12 +1,12 @@
-import SwaggerUI from 'swagger-ui'
+import { SwaggerUIBundle } from 'swagger-ui-dist'
 import ApiKeyFormPlugin from '@/lib/swagger_api_key_form';
 import domready from 'domready';
-import 'swagger-ui/dist/swagger-ui.css';
+import 'swagger-ui-dist/swagger-ui.css';
 
 domready(() => {
   const swaggerUiEl = document.getElementById('swagger-ui');
 
-  SwaggerUI({
+  SwaggerUIBundle({
     url: swaggerUiEl.dataset.swaggerUrl,
     dom_id: '#swagger-ui',
     plugins: [
